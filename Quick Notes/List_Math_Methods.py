690357566092329ff2dff7c320889d0869da2811
YoungAmbitious <indy500media@gmail.com>
2023-06-04 07:36:20 -0500

    Create List_Math_Methods.py

---
+++ Quick Notes/List_Math_Methods.py
@@ -0,0 +1,17 @@
+''' List Math Methods '''
+
+numList = [12, 456, 234, 987, 6, 75]
+
+# ADDING THE VALUES OF LIST ELEMENTS
+# sum()
+print(sum(numList))
+#Output: 1770
+
+# HIGHEST AND LOWEST VALUE IN THE LIST
+# min(), max()
+
+print(min(numList)) #Output: 6
+
+print(max(numList)) # Output: 987
+
+
